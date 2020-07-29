8e51d6d2b9f120a6f4133227a84200c83e1f0270
JSS <choss01@oneplat.co>
2020-07-29 15:25:37 +0900

    배치 구현 성공

---
+++ src/main/java/com/example/demo/BatchController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class BatchController {
+  
+  private final BatchRepository batchRepository;
+  
+  @GetMapping("/test")
+  public String test() {
+    log.info(batchRepository.findById((long) 1).toString());
+    return "Success";
+//    return "fail";
+  }
+
+}
