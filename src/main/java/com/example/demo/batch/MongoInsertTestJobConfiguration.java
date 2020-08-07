2458aa02d902ade1e2df7835564b51cfe0debc62
JSS <choss01@oneplat.co>
2020-08-07 16:23:20 +0900

    지금까지는 1차 성능 테스트 끝냈다 근데 더 해야될것 같아 너무 오래걸려

--- src/main/java/com/example/demo/batch/MongoInsertTestJobConfiguration.java
+++ src/main/java/com/example/demo/batch/MongoInsertTestJobConfiguration.java
@@ -6,17 +6,13 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.batch.item.data.MongoItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import com.example.demo.db.SmartChoiceEntity;
 import com.example.demo.db.SmartChoiceRepository;
@@ -32,7 +28,7 @@ public class MongoInsertTestJobConfiguration {
   private final SmartChoiceRepository smartChoiceRepository;
   private final MongoTemplate mongoTemplate;
 
-  private int chunkSize = 10;
+  private int chunkSize = 500000;
 
   @Bean
   public Job mongoInsertTestJob() {
@@ -48,14 +44,15 @@ public class MongoInsertTestJobConfiguration {
   }
 
   @Bean
-  public ItemReader<SmartChoiceEntity> mongoItemReader(){
+  public MongoItemReader<SmartChoiceEntity> mongoItemReader(){
     MongoItemReader<SmartChoiceEntity> mongoItemReader = new MongoItemReader<>();
     mongoItemReader.setTemplate(mongoTemplate);
-    mongoItemReader.setCollection("SmartChoice");
+    mongoItemReader.setCollection("SmartChoiceDummy");
     mongoItemReader.setQuery("{}");
     Map<String, Sort.Direction> sort = new HashMap<>();
-    sort.put("status", Sort.Direction.ASC);
+    sort.put("_id", Sort.Direction.ASC);
     mongoItemReader.setSort(sort);
+    mongoItemReader.setPageSize(500000);
     mongoItemReader.setTargetType(SmartChoiceEntity.class);
     
     return mongoItemReader;
@@ -81,7 +78,7 @@ public class MongoInsertTestJobConfiguration {
   
   
   @Bean 
-  public ItemWriter<SmartChoiceEntity> mongoItemWriter(){
+  public MongoItemWriter<SmartChoiceEntity> mongoItemWriter(){
     MongoItemWriter<SmartChoiceEntity> mongoItemWriter = new MongoItemWriter<>();
     mongoItemWriter.setTemplate(mongoTemplate);
     mongoItemWriter.setCollection("batchTest");
